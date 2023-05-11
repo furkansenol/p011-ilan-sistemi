d0826db9a02239ab688c5e9a924e54edf3b3cff8
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-05-11 13:18:42 +0300

    BusinessKatmanı

---
+++ IlanSistemi/IlanSistemi.Business/Concrete/PageManager.cs
@@ -0,0 +1,51 @@
+﻿using IlanSistemi.Business.Abstract;
+using IlanSistemi.DataAccess.Abstract;
+using IlanSistemi.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlanSistemi.Business.Concrete
+{
+    public class PageManager : IPageService
+    {
+        IPageDal _pageDal;
+
+        public PageManager(IPageDal pageDal)
+        {
+            _pageDal = pageDal;
+        }
+
+        public void TAdd(Page t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Page t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Page TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Page> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Page> TGetListbyFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Page t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IlanSistemi/IlanSistemi.Business/Concrete/SettingManager.cs
@@ -0,0 +1,51 @@
+﻿using IlanSistemi.Business.Abstract;
+using IlanSistemi.DataAccess.Abstract;
+using IlanSistemi.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlanSistemi.Business.Concrete
+{
+    public class SettingManager : ISettingService
+    {
+        ISettingDal _settingDal;
+
+        public SettingManager(ISettingDal settingDal)
+        {
+            _settingDal = settingDal;
+        }
+
+        public void TAdd(Setting t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(Setting t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Setting TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Setting> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Setting> TGetListbyFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(Setting t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
