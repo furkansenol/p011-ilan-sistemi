2d2bcc71d2d785332a728651f36a0e3e66af217f
furkansenol <furkansenol97@gmail.com>
2023-05-12 14:10:41 +0300

    added layers for User model

---
+++ IlanSistemi/IlanSistemi.Business/Concrete/UserManager.cs
@@ -0,0 +1,51 @@
+﻿using IlanSistemi.Business.Abstract;
+using IlanSistemi.DataAccess.Abstract;
+using IlanSistemi.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlanSistemi.Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+
+        public void TAdd(User t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TDelete(User t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public User TGetByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> TGetList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<User> TGetListbyFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(User t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ IlanSistemi/IlanSistemi.DataAccess/Migrations/20230512105955_UserModelImplementation.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IlanSistemi.DataAccess.Migrations
+{
+    public partial class UserModelImplementation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
