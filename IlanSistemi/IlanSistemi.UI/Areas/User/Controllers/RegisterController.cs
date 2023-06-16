f8df410d6d67991a860a9344a68ffd947f204bd9
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-06-16 15:51:41 +0300

    User Paneli düzenleme

---
+++ IlanSistemi/IlanSistemi.UI/Areas/User/Controllers/RegisterController.cs
@@ -0,0 +1,66 @@
+﻿using IlanSistemi.Business.Concrete;
+using IlanSistemi.Entities.Concrete;
+using IlanSistemi.UI.Areas.Admin.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IlanSistemi.UI.Areas.Admin.Controllers
+{
+
+    [AllowAnonymous]
+    [Route("User/[controller]/[action]")]
+    [Area("User")]
+    public class RegisterController : Controller
+    {
+        private readonly UserManager<AdminUser> _userManager;
+
+        public RegisterController(UserManager<AdminUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(UserRegisterViewModel p)
+        {
+            if (ModelState.IsValid)
+            {
+                AdminUser w = new AdminUser()
+                {
+                    Name = p.Name,
+                    Surname = p.Surname,
+                    Email = p.Mail,
+                    UserName = p.UserName,
+                    ImageUrl = p.ImagURL = "/Tamplate/images/profil.jpg/"
+
+				};
+
+                if (p.Password == p.ConfirmPassword)
+                {
+                    var result = await _userManager.CreateAsync(w, p.Password);
+
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index", "Login");
+                    }
+                    else
+                    {
+                        foreach (var item in result.Errors)
+                        {
+                            ModelState.AddModelError("", item.Description);
+                        }
+                    }
+
+                }
+            }
+            return View();
+        }
+
+    }
+}
---
+++ IlanSistemi/IlanSistemi.UI/Areas/User/Controllers/UrunController.cs
@@ -0,0 +1,104 @@
+﻿using IlanSistemi.Business.Concrete;
+using IlanSistemi.DataAccess.Concrete;
+using IlanSistemi.DataAccess.EntityFramework;
+using IlanSistemi.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IlanSistemi.UI.Areas.Admin.Controllers
+{
+	[Area("User")]
+	[Route("User/[controller]/[action]")]
+	public class UrunController : Controller
+	{
+		private readonly AdvertManager advertManager;
+
+		public UrunController()
+		{
+			advertManager = new AdvertManager(new EfAdvertDal());
+		}
+
+		public IActionResult Index()
+		{
+			var values = advertManager.TGetList();
+			return View(values);
+		}
+
+		[HttpGet]
+		public IActionResult EditAdvert(int id)
+		{
+			var advert = advertManager.TGetByID(id);
+
+			if (advert == null)
+			{
+				// İstenilen ID'ye sahip reklam bulunamadı, gerekli işlemleri yapabilirsiniz.
+				return RedirectToAction("Index");
+			}
+
+			return View(advert);
+		}
+
+		[HttpPost]
+		public IActionResult EditAdvert(Advert advert)
+		{
+			advert.UpdatedAt = DateTime.Now;
+			advertManager.TUpdate(advert);
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult AddAdvert() 
+		{
+			return View(); 
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> AddExperience(Advert advert, IFormFile picture)
+		{
+			if (advert.CreatedAt == null)
+			{
+				advert.CreatedAt = DateTime.Now;
+			}
+
+			advertManager.TAdd(advert);
+
+			if (picture != null)
+			{
+				var extension = Path.GetExtension(picture.FileName);
+				var imageName = Guid.NewGuid() + extension;
+				var saveLocation = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot/Productimage", imageName);
+
+				using (var stream = new FileStream(saveLocation, FileMode.Create))
+				{
+					await picture.CopyToAsync(stream);
+				}
+
+				var advertImage = new AdvertImage
+				{
+					ImagePath = imageName,
+					
+				};
+
+				if (advert.advertImages == null)
+				{
+					advert.advertImages = new List<AdvertImage>();
+				}
+
+				advert.advertImages.Add(advertImage);
+			}
+
+			return RedirectToAction("Index");
+		}
+
+		public IActionResult DeleteAdvert(int id)
+		{
+			var values = advertManager.TGetByID(id);
+			advertManager.TDelete(values);
+			return RedirectToAction("Index");
+		}
+
+
+
+
+
+	}
+
+}
