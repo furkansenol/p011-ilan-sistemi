0129e52256560a0eee4ffc7d576d102ec1a98e60
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-06-17 18:56:07 +0300

    Değişiklikler yapıldı

---
+++ IlanSistemi/IlanSistemi.UI/Areas/Admin/Controllers/DashboardController.cs
@@ -0,0 +1,20 @@
+﻿using IlanSistemi.DataAccess.Concrete;
+using IlanSistemi.Entities.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Xml.Linq;
+
+namespace IlanSistemi.UI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/[controller]/[action]")]
+	public class DashboardController : Controller
+	{
+		public IActionResult Index()
+		{
+
+
+			return View();
+		}
+	}
+}
