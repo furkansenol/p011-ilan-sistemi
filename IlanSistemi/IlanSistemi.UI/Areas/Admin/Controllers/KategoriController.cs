25cea37fb3ebf7497e6a63ce9b3867bb6cda2bdb
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-06-17 22:17:39 +0300

    Admin Paneli Düzenleme

---
+++ IlanSistemi/IlanSistemi.UI/Areas/Admin/Controllers/KategoriController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IlanSistemi.UI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/[controller]/[action]")]
+	public class KategoriController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ IlanSistemi/IlanSistemi.UI/Areas/Admin/Controllers/KullaniciController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IlanSistemi.UI.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	[Route("Admin/[controller]/[action]")]
+	public class KullaniciController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
