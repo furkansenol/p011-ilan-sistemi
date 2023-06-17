211cdfee1bab0473799ee2caed78e75a32c69da7
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-06-17 19:58:51 +0300

    Admin Paneli düzenlemeleri

---
+++ IlanSistemi/IlanSistemi.UI/Areas/Admin/ViewComponents/Dashboard/ProdoctList.cs
@@ -0,0 +1,18 @@
+﻿using IlanSistemi.Business.Concrete;
+using IlanSistemi.DataAccess.EntityFramework;
+using IlanSistemi.Entities.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IlanSistemi.UI.Areas.Admin.ViewComponents.Dashboard
+{
+	public class ProdoctList : ViewComponent
+	{
+		AdvertManager _advertManager = new AdvertManager(new EfAdvertDal());
+		public IViewComponentResult Invoke()
+		{
+			var values = _advertManager.TGetList();
+			return View(values);
+			
+		}
+	}
+}
---
+++ IlanSistemi/IlanSistemi.UI/Areas/Admin/ViewComponents/Dashboard/UserList.cs
@@ -0,0 +1,17 @@
+﻿using IlanSistemi.Business.Concrete;
+using IlanSistemi.DataAccess.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IlanSistemi.UI.Areas.Admin.ViewComponents.Dashboard
+{
+    public class UserList : ViewComponent
+    {
+        UserManager _userManager = new UserManager(new EfUserDal());
+
+        public IViewComponentResult Invoke()
+        {
+            var values = _userManager.TGetList();
+            return View(values);    
+        }
+    }
+}
