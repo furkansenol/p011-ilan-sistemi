33f657f3f0f9da4c41743d3a133ebbb410638c1a
Serkan Kılıçkap <serkankilickap@gmail.com>
2023-06-23 01:46:32 +0300

    about & contact

---
+++ IlanSistemi/IlanSistemi.UI/Controllers/StaticController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IlanSistemi.UI.Controllers
+{
+    public class StaticController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
